16e909ff523007a82d31e017a36f73537e208ae5
NIhal <jaisnj541@gmail.com>
2024-10-16 22:59:15 +0530

    Fixed navigation link in RelatedDoctor component and added smooth scrolling on doctor card click. Enhanced UI with hover and

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams} from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctor from '../components/RelatedDoctor'
 
 const Appointments = () => {
   const {docId} = useParams()
@@ -138,7 +139,8 @@ const Appointments = () => {
      <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>   
 </div>
 
-
+  {/*----Listing Related Doctor---*/}
+  <RelatedDoctor docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
