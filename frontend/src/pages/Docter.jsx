618e0f5b8a3579978d58b87df2978e3fb56b909b
NIhal <jaisnj541@gmail.com>
2024-10-18 23:12:23 +0530

    Add filter toggle button with responsive visibility

--- frontend/src/pages/Docter.jsx
+++ frontend/src/pages/Docter.jsx
@@ -6,6 +6,7 @@ const Docter = () => {
   const {speciality} =useParams()
   const[filterDoc,setFilterDoc]=useState([])
   const navigate = useNavigate();
+  const [showFilter,setShowFilter]=useState(false)
   const {doctors}=useContext(AppContext);
 
   const applyFilter = () => {
@@ -25,7 +26,8 @@ const Docter = () => {
     <div>
         <p className='text-gray-600'>Browse through the doctors specialist</p>
         <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-          <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ?'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filter</button>
+          <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden sm:flex'}`}>
             <p onClick={()=> speciality === 'General physician' ? navigate('/doctors'):navigate('/doctors/General physician')}  className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician"?"bg-indigo-100 text-black":"" }`}>General Physician</p>
 
             <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors'):navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist"?"bg-indigo-100 text-black":"" }`}>Gynecologist</p>
