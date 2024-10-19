a556a604dbfeb46379fdc32de099f26934f0a5d5
NIhal <jaisnj541@gmail.com>
2024-10-19 17:34:30 +0530

    Add multer configuration for file uploads

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,15 @@
+
+
+// Api for admin doctor
+
+const addDoctor = async(req,res) => 
+    {
+      try {
+        
+        const {name, email, password, speciality, degree, experience, about, fees, address} =req.body;  
+
+      } catch (error) {
+        
+      }
+
+    }
