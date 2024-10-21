39506d4fd4171e853ccaa964dbbcf2f091f7c323
NIhal <jaisnj541@gmail.com>
2024-10-21 15:30:04 +0530

    Add logout functionality and role display to Navbar

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,32 +1,93 @@
-import { useState } from "react"
-import {assets} from "../assets/assets.js"
+import { useContext, useState } from "react";
+import { assets } from "../assets/assets.js";
+import { AdminContext } from "../context/AdminContext.jsx";
+import axios from "axios";
+import { toast } from "react-toastify";
+
 
 const Login = () => {
+  const [state, setState] = useState("Admin");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAToken, backendUrl } = useContext(AdminContext);
+  
 
-    const [state,setState] = useState('Admin')
-  return (
-    <from className='min-h-[80vh] flex items-center'>
-        <div className=" flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5ESE] text-sm shadow-lg">
-        <p className="text-2xl font-semibold m-auto"><span className="text-primary">{state}</span>Login</p>
-        <div className="w-full">
-            <p>Email</p>
-            <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="email" required/>
-        </div>
-        <div className="w-full">
-            <p>Password</p>
-            <input className="border border-[#DADADA] rounded w-full p-2 mt-1"  type="password" required/>
-        </div>
-        <buttom className='bg-primary text-white w-full py-2 rounded-md text-center'>Login</buttom>
-        {
-          state === 'Admin'
-          ?
-          <p>Doctor Login?<span className="text-primary underline cursor-pointer" onClick={()=>setState('Doctor')}>Click here</span></p>
-          :
-          <p>Admin Login?<span  className="text-primary underline cursor-pointer" onClick={()=>setState('Admin')}>Click here</span></p>
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (state === "Admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem('aToekn',data.token)
+          setAToken(data.token);
         }
-        </div>
-    </from>
-  )
-}
+        else{
+          toast.error(data.message)
+        }
+      } else {
+        // Handle doctor login case here
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default Login
+  return (
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5ESE] text-sm shadow-lg">
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-primary">{state}</span> Login
+        </p>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
+        </div>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-center">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login?
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
