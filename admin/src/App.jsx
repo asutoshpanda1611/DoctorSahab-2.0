e43573a699b7157f44aab27fb0cab32286a90620
NIhal <jaisnj541@gmail.com>
2024-10-21 16:18:16 +0530

    Fix dynamic class handling in Sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorList from './pages/Admin/DoctorList';
 
 
   const App = () => {
@@ -13,6 +19,18 @@ import Navbar from './components/Navbar';
     <div className='bg-[#F8F9FD]'>
      <ToastContainer/>
      <Navbar/>
+     <div className='flex items-start'>
+      <Sidebar/>
+      <Routes>
+        <Route path='/'/>
+        <Route path='/admin-dashboard' element={<Dashboard/>}/>
+        <Route path='/all-appointments' element={<AllAppointments/>}/>
+        <Route path='/all-doctor' element={<AddDoctor/>}/>
+        <Route path='/doctor-list' element={<DoctorList/>}/>
+        
+
+      </Routes>
+     </div>
      
     </div>
   ): (
