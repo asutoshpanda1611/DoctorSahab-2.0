546b09d178fabf67e4db5932b4d9aded4c152f51
NIhal <jaisnj541@gmail.com>
2024-10-21 08:35:34 +0530

    Fix typo in form tag and add state toggle between Admin and Doctor in Login component

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useState } from "react"
+import {assets} from "../assets/assets.js"
 
 const Login = () => {
+
+    const [state,setState] = useState('Admin')
   return (
-    <div>Login</div>
+    <from className='min-h-[80vh] flex items-center'>
+        <div className=" flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5ESE] text-sm shadow-lg">
+        <p className="text-2xl font-semibold m-auto"><span className="text-primary">{state}</span>Login</p>
+        <div className="w-full">
+            <p>Email</p>
+            <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="email" required/>
+        </div>
+        <div className="w-full">
+            <p>Password</p>
+            <input className="border border-[#DADADA] rounded w-full p-2 mt-1"  type="password" required/>
+        </div>
+        <buttom className='bg-primary text-white w-full py-2 rounded-md text-center'>Login</buttom>
+        {
+          state === 'Admin'
+          ?
+          <p>Doctor Login?<span className="text-primary underline cursor-pointer" onClick={()=>setState('Doctor')}>Click here</span></p>
+          :
+          <p>Admin Login?<span  className="text-primary underline cursor-pointer" onClick={()=>setState('Admin')}>Click here</span></p>
+        }
+        </div>
+    </from>
   )
 }
 
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#5F6FFF'      }
+    },
   },
   plugins: [],
 }
